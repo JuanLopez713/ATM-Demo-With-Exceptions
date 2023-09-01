ae7b950acf12badc1775ef4a3edec278856e78b6
Juan Lopez <juanlpz713@gmail.com>
2023-08-31 17:13:49 -0700

    Added JSON Objects

--- Tester.java
+++ Tester.java
@@ -1,34 +1,36 @@
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Tester {
     // Tester
-    public static void main(String[] args) throws ATMError, FileNotFoundException {
+    public static void main(String[] args) throws ATMError, IOException {
 
-        ATM.openAccount("Juan", "123@gmail.com", 10.00);
-        ATM.openAccount("Theiss", "456@gmail.com", 10.00);
-        ATM.checkBalance("Juan");
-        ATM.checkBalance("Theiss");
-
-        ATM.depositMoney("Juan", 30);
-
-        ATM.checkBalance("Juan");
-
-        ATM.transferMoney("Theiss", "Juan", 10);
-
-        ATM.checkBalance("Juan");
-        ATM.checkBalance("Theiss");
-
-        // ERRORS
-        ATM.closeAccount("Lopez");
-        ATM.openAccount("Juan", "123@gmail.com", 10);
-        ATM.checkBalance("Lopez");
-        ATM.closeAccount("Juan");
-        ATM.depositMoney("Lopez", 10);
-        ATM.withdrawMoney("Lopez", 1000);
-        ATM.withdrawMoney("Juan", 1000);
-        ATM.transferMoney("Lopez", "Juan", 10);
-        ATM.transferMoney("Theiss", "Juan", 110000);
-
+        ATM.bootup();
         ATM.audit();
+        // ATM.openAccount("Juan", "123@gmail.com", 10.24);
+        // ATM.openAccount("Theiss", "456@gmail.com", 10.76);
+        // ATM.checkBalance("Juan");
+        // ATM.checkBalance("Theiss");
+
+        // ATM.depositMoney("Juan", 30);
+
+        // ATM.checkBalance("Juan");
+
+        // ATM.transferMoney("Theiss", "Juan", 10);
+
+        // ATM.checkBalance("Juan");
+        // ATM.checkBalance("Theiss");
+
+        // // ERRORS
+        // ATM.closeAccount("Lopez");
+        // ATM.openAccount("Juan", "123@gmail.com", 10);
+        // ATM.checkBalance("Lopez");
+        // ATM.closeAccount("Juan");
+        // ATM.depositMoney("Lopez", 10);
+        // ATM.withdrawMoney("Lopez", 1000);
+        // ATM.withdrawMoney("Juan", 1000);
+        // ATM.transferMoney("Lopez", "Juan", 10);
+        // ATM.transferMoney("Theiss", "Juan", 110000);
+
+        // ATM.audit();
     }
 }
